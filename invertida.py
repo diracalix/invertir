07d7fd64aef1894fde6e2d2bb310503efd1879ae
diracalix <brizuelanando@gmail.com>
2024-06-11 16:21:50 -0400

    Invertir una cadena de caracteres

---
+++ invertida.py
@@ -0,0 +1,10 @@
+
+def invertir_cadena(cadena):
+    return cadena[::-1]
+
+
+cadena_usuario = input("Ingrese una cadena de caracteres: ")
+
+cadena_invertida = invertir_cadena(cadena_usuario)
+
+print("Cadena invertida:", cadena_invertida)
