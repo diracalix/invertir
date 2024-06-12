279f1e7d5e70fa49eef2817f62b2378566255226
diracalix <brizuelanando@gmail.com>
2024-06-11 20:44:46 -0400

    invertida.py

--- invertida.py
+++ invertida.py
@@ -1,7 +1,9 @@
 
 def invertir_cadena(cadena):
-    return cadena[::-1]
-
+    cadena_invertida = ""
+    for caracter in cadena:
+        cadena_invertida = caracter + cadena_invertida
+    return cadena_invertida
 
 cadena_usuario = input("Ingrese una cadena de caracteres: ")
 
